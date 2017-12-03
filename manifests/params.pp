a6d114a621871b866fc4c572274d680f3bb61ef2
jpadams <jeremy@puppetlabs.com>
2017-12-02 22:03:23 -0800

    use puppet_gem for PE

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,9 +2,9 @@
 # vCenter common parameters
 class vcenter::params {
 
-  if $::puppetversion =~ /Puppet Enterprise/ {
-    $provider  = 'pe_gem'
-    $ruby_path = '/opt/puppet/bin/ruby'
+  if $facts['pe_server_version'] {
+    $provider  = 'puppet_gem'
+    $ruby_path = '/opt/puppetlabs/puppet/bin/ruby'
   } else {
     $provider  = 'gem'
     $ruby_path = '/usr/bin/env ruby'
